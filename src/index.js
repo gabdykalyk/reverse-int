bca81d9dd038f3b3b936700a2682e85634114e09
gabdykalyl <gabdykalyk@gmail.com>
2021-03-15 17:33:41 +0600

    feat: reverse-int finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n * -1
+        n = n + "";
+	return n.split("").reverse().join("");  
+    } else {
+        n = n + "";
+        return n.split("").reverse().join("");  
+    }
 }
